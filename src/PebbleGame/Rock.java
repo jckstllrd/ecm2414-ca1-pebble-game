3f2230830e4898597f06079a3f5eb987b0d41365
Jack Stallard <jackstallard17@gmail.com>
2021-10-19 16:03:09 +0100

    Rock Constructor Added

--- src/PebbleGame/Rock.java
+++ src/PebbleGame/Rock.java
@@ -4,4 +4,11 @@ public class Rock {
     
     int weight;
 
+    /**
+     * The Constructor for the Rock object
+     * @param weight Inputted weight
+     */
+    public Rock(int weight) {
+        this.weight = weight;
+    }
 }
