24fbf790d6b3923034af2877cbf9fa0eb4bb9dad
Treeve <treevew@gmail.com>
2021-10-19 16:14:10 +0100

    Created exception

---
+++ src/PebbleGame/InvalidInputException.java
@@ -0,0 +1,13 @@
+package PebbleGame;
+
+public class InvalidInputException extends Exception{
+
+    public InvalidInputException() {
+
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+    
+}
